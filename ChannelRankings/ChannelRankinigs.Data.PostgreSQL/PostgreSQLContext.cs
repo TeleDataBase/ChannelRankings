9e2439b55bb91d55388097e315e99a8736a0dbef
Daniel Georgiev <bgdopus@live.com>
2017-06-08 01:16:26 +0300

    Postgre setup

---
+++ ChannelRankings/ChannelRankinigs.Data.PostgreSQL/PostgreSQLContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+
+namespace ChannelRankinigs.Data.PostgreSQL
+{
+    public class PostgreSQLContext : DbContext
+    {
+
+        public PostgreSQLContext() : base("NpgsqlConnectionString")
+        {
+
+        }
+
+        public virtual DbSet<Company> CompanySet { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("public");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
