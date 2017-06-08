2eb0228917cc03d7cd43efb7fa6b3db87ec18309
Daniel Georgiev <bgdopus@live.com>
2017-06-09 00:21:46 +0300

    Finnaly working

--- ChannelRankings/Source/ChannelRankings.Data.PostgreSQL/Migrations/201706082118389_first.Designer.cs
+++ ChannelRankings/Source/ChannelRankings.Data.PostgreSQL/Migrations/201706082118389_first.Designer.cs
@@ -13,7 +13,7 @@ namespace ChannelRankings.Data.PostgreSQL.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201706081935263_first"; }
+            get { return "201706082118389_first"; }
         }
         
         string IMigrationMetadata.Source
