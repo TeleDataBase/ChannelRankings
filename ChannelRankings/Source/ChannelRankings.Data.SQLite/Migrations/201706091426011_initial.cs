db8f102b51ddd983fe33f1e110a0ade25bd1f13e
Daniel Georgiev <bgdopus@live.com>
2017-06-09 17:29:47 +0300

    sqlite working. data uploaded

---
+++ ChannelRankings/Source/ChannelRankings.Data.SQLite/Migrations/201706091426011_initial.cs
@@ -0,0 +1,28 @@
+namespace ChannelChannelRankings.Data.SQLite.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.YouTubeChannels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 30),
+                        Url = c.String(maxLength: 64),
+                        Rank = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.YouTubeChannels");
+        }
+    }
+}
--- ChannelRankings/Source/ChannelRankings.Data.SQLite/Migrations/SqliteConfiguration.cs
+++ ChannelRankings/Source/ChannelRankings.Data.SQLite/Migrations/SqliteConfiguration.cs
@@ -1,11 +1,10 @@
+using ChannelRankings.Data.SQLite;
+using ChannelRankings.Data.SQLite.Entity;
 using System.Data.Entity.Migrations;
 using System.Data.SQLite.EF6.Migrations;
 
-/// <summary>
-/// 作者：音樂咖啡
-/// 联系方式：QQ 136463644
-/// </summary>
-namespace Sqlitecodefirst.Migrations
+
+namespace ChannelChannelRankings.Data.SQLite.Migrations
 {
     internal sealed class ContextMigrationConfiguration : DbMigrationsConfiguration<SqliteDBContext>
     {
@@ -19,7 +18,19 @@ namespace Sqlitecodefirst.Migrations
         protected override void Seed(SqliteDBContext context)
         {
             //base.Seed(context);
-            context.Yusers.AddOrUpdate(new Entity.YUser[] { new Entity.YUser() {  UserID=1,UserName="test1", UserSex="man" ,NickName="tetdd"} });
+            //context.Yusers.AddOrUpdate(new Entity.YUser[] { new Entity.YUser() {  UserID=1,UserName="test1", UserSex="man" ,NickName="tetdd"} });
+
+           context.YouTubeChannels.AddOrUpdate(new YouTubeChannel[]
+            {
+                new YouTubeChannel() { Name = "TUSofia", Url = "youtube.com/tusofia", Rank = 7},
+                new YouTubeChannel() { Name = "FMI", Url = "youtube.com/fmi", Rank = 71},
+                new YouTubeChannel() { Name = "TelerikAcademy", Url = "youtube.com/telerikacademy", Rank = 1},
+                new YouTubeChannel() { Name = "Funny", Url = "youtube.com/funny", Rank = 2},
+                new YouTubeChannel() { Name = "Kango", Url = "youtube.com/kango", Rank = 11},
+                new YouTubeChannel() { Name = "UFC", Url = "youtube.com/ufc", Rank = 3},
+                new YouTubeChannel() { Name = "Camp", Url = "youtube.com/camp", Rank = 25},
+                new YouTubeChannel() { Name = "Shooting", Url = "youtube.com/shooting", Rank = 18},
+            });
         }
     }
 }
